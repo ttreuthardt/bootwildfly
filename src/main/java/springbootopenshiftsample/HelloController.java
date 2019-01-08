461555f5fe952255f39c00641e0aa1c1b53deeac
Thomas Treuthardt <you@example.com>
2019-01-08 22:36:11 +0100

    update README and hello endpoint

--- src/main/java/springbootopenshiftsample/HelloController.java
+++ src/main/java/springbootopenshiftsample/HelloController.java
@@ -19,7 +19,7 @@ public class HelloController {
             e.printStackTrace();
         }
 
-        return ("Hello, Docker Academy on Wildfly 10.1 from " + hostName);
+        return ("Hello, Docker Academy. You hit the pod -> " + hostName);
     }
 
 }
