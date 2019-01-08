cca4f57e405a3978a68f34e5bfdd8d2de036e983
Thomas Treuthardt <you@example.com>
2019-01-08 23:45:22 +0100

    update to standalone jar file spring boot 2.1.1

--- src/main/java/springbootopenshiftsample/Application.java
+++ src/main/java/springbootopenshiftsample/Application.java
@@ -1,16 +1,12 @@
 package springbootopenshiftsample;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
-@EnableAutoConfiguration
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
